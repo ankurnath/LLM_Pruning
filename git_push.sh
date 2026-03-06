cb31d6a8511fb577cf67398b449a5e55d832b1be
Ankur Nath <anath@csce-44q58y3.engr.tamu.edu>
2026-03-06 02:25:33 -0600

    Updated files

--- git_push.sh
+++ git_push.sh
@@ -1,3 +1,7 @@
-git add .
+#!/bin/bash
+# Add all files under 50MB
+find . -not -path './.git/*' -type f -size -50M | while read f; do
+    git add "$f"
+done
 git commit -m "Updated files"
 git push origin rebuttal
